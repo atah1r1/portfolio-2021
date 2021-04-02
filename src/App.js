721b1298089852f05efc7f366cbd5f11259414e9
Amine Tahiri <atahiri@student.1337.ma>
2021-04-02 17:48:11 +0100

    OKOK

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import {Wrapper, Title} from './test.js';
+import {useContext} from 'react';
+// import { Link } from 'gatsby'
+// import { Container, Logo } from 'components/common'
+// import { ThemeContext } from 'providers/ThemeProvider'
+// import NavbarLinks from '../NavbarLinks'
+import { Wrapper, BrandLogo, Brand } from './test'
 
-function App() {
+export default () => {
+  const { theme } = useContext(ThemeContext)
   return (
-    <Wrapper>
-    <Title>
-      Hello World!
-    </Title>
-  </Wrapper>
-  );
+    <Wrapper as={Container}>
+      <Brand as={Link} theme={theme} to="/">
+        <BrandLogo
+          as={Logo}
+          color={theme === 'dark' ? '#fff' : '#212121'}
+          strokeWidth="2"
+        />
+        Smakosh
+      </Brand>
+      <NavbarLinks desktop />
+    </Wrapper>
+  )
 }
-
-export default App;
--- src/test.js
+++ src/test.js
@@ -1,12 +1,33 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+export const Wrapper = styled.div`
+  padding: 0.8rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
-export const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
+export const Brand = styled.div`
+  display: flex;
+  align-items: center;
+  color: #212121;
+
+  ${({ theme }) =>
+    theme === 'dark' &&
+    `
+			color: #fff;
+	`};
+`
+
+export const BrandLogo = styled.div`
+  transition: 0.5s;
+  height: 2.8rem;
+  width: 2.8rem;
+  margin-bottom: 0;
+  margin-right: 0.5rem;
+
+  &:hover {
+    opacity: 0.7;
+    transition: 0.5s;
+    transform: rotate(360deg);
+  }`
